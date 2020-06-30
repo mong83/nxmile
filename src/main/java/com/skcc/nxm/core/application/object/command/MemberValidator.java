a277947897ec41de888dcf5e692b4b0056e725eb
mong83 <06007sk@gmail.com>
2020-07-01 03:56:38 +0900

    1.필수값, 자리수, 입력값패턴검증

--- src/main/java/com/skcc/nxm/core/application/object/command/MemberValidator.java
+++ src/main/java/com/skcc/nxm/core/application/object/command/MemberValidator.java
@@ -14,6 +14,11 @@ public class MemberValidator {
         if(requestDto.getSearchFg().equals("1") && requestDto.getMbrId()==null) {
             System.out.println("validate - err"+"["+requestDto.getSearchFg()+"]"+"["+requestDto.getMbrId()+"]");
             throw new BusinessException("NotFound", ErrorCode.EMPTY_MBRID);
+
+        }else if(requestDto.getSearchFg().equals("2") && requestDto.getMbrFg()==null){
+
+            throw new BusinessException("NotFound", ErrorCode.EMPTY_MBRFG);
+
         }
 
             System.out.println("validate - success" + "[" + requestDto.getSearchFg() + "]" + "[" + requestDto.getMbrId() + "]");
